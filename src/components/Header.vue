b807817872b0a147b7eaf63dd742d8e6e4508e34
Karen <ksuen.dev@gmail.com>
2022-01-30 00:49:56 +0000

    feat(header): add font awesome virus icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
 
     <p>
       API by
